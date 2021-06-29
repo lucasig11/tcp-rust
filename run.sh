d95e5459567f1180cd772c4beb539d0cc44b57db
lucas <lucasfonsecard@icloud.com>
2021-06-29 01:49:05 -0300

    feat: filter protocols

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cargo b --release
+ext=$?
+if [ $ext -ne 0 ]; then
+	exit $ext
+fi
+sudo setcap CAP_NET_ADMIN=eip target/release/tcp_rust
+./target/release/tcp_rust &
+pid=$!
+sudo ip addr add 192.168.0.1/24 dev tun0
+sudo ip link set up dev tun0
+trap "kill $pid" INT TERM
+wait $pid
